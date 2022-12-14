ff6cf23496ad9da6c297b7ccc9ab22e82d9d7d44
Yilmaz Yagiz Dokumaci <dokumaciyagiz@gmail.com>
2022-12-14 23:21:08 +0300

    updated apptextstyles

--- lib/utils/app_textstyles.dart
+++ lib/utils/app_textstyles.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
+import 'app_colors.dart';
+
 class AppTextStyle {
   AppTextStyle._();
 
@@ -8,6 +10,25 @@ class AppTextStyle {
     return TextStyle(
       color: Colors.white,
       fontSize: 22.sp,
+      fontWeight: FontWeight.w700,
+      letterSpacing: -0.2.sp,
+      overflow: TextOverflow.ellipsis,
+    );
+  }
+
+  static TextStyle loginViewTitle() {
+    return TextStyle(
+      color: Colors.white,
+      fontSize: 22.sp,
+      fontWeight: FontWeight.w700,
+      letterSpacing: -0.2.sp,
+      overflow: TextOverflow.ellipsis,
+    );
+  }
+  static TextStyle loginBtnTitle() {
+    return TextStyle(
+      color: AppColors.blackBackground,
+      fontSize: 18.sp,
       fontWeight: FontWeight.w700,
       letterSpacing: -0.2.sp,
       overflow: TextOverflow.ellipsis,
