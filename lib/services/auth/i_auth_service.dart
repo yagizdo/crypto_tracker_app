7c9b7889ddab2d4f7e8198673d3bc034bd7ef576
Yilmaz Yagiz Dokumaci <dokumaciyagiz@gmail.com>
2022-12-14 22:38:01 +0300

    added:IAuthService

---
+++ lib/services/auth/i_auth_service.dart
@@ -0,0 +1,12 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+abstract class IAuthService{
+  Future<void> reloadUser();
+  Future<void> logout();
+  Future<void> sendVerificationMail();
+  Future<void> sendResetPasswordMail();
+  Stream<User?>? authStateChanges();
+  Future<User?> registerWithEmailAndPassword({required String userEmail, required String userPassword});
+  Future<User?> signInWithEmailAndPassword({required String userEmail, required String userPassword});
+  Future<User?> signInWithGoogle();
+}
