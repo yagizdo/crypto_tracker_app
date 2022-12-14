7c706e87ea5826b28e48885e110d522e3db20010
Yilmaz Yagiz Dokumaci <dokumaciyagiz@gmail.com>
2022-12-14 21:00:05 +0300

    added: Auth view and auth textfield

---
+++ lib/views/auth_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AuthView extends StatefulWidget {
+  const AuthView({Key? key}) : super(key: key);
+
+  @override
+  State<AuthView> createState() => _AuthViewState();
+}
+
+class _AuthViewState extends State<AuthView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
