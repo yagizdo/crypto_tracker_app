e5e1e9b882502a09c451943c4db1ff5f4c0d9fb7
Yilmaz Yagiz Dokumaci <dokumaciyagiz@gmail.com>
2022-12-14 21:08:31 +0300

    added validator and register component

---
+++ lib/services/validator.dart
@@ -0,0 +1,28 @@
+class Validator {
+  static String? email(String? value) {
+    if (RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+").hasMatch(value!)) {
+      return null;
+    }
+    return 'Please enter a valid email';
+  }
+
+  static String? password(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Please enter some text';
+    } else if (value.length < 6) {
+      return 'Password must be at least 6 characters';
+    }
+    return null;
+  }
+
+  static String? passwordsMatch(String? value, String? passwordOne, String? passwordTwo) {
+    if (value == null || value.isEmpty) {
+      return 'Please enter some text';
+    } else if (value.length < 6) {
+      return 'Password must be at least 6 characters';
+    } else if (passwordOne != passwordTwo) {
+      return 'Passwords do not match';
+    }
+    return null;
+  }
+}
