4a3620550aa6f0058fa9436c5868eeb86b2ba80a
Yilmaz Yagiz Dokumaci <dokumaciyagiz@gmail.com>
2022-12-15 00:40:10 +0300

    updated social auth buttons

--- lib/widgets/social_auth_btns.dart
+++ lib/widgets/social_auth_btns.dart
@@ -1,4 +1,6 @@
+import 'package:crypto_tracker/utils/app_constants.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:sign_button/sign_button.dart';
 
 import '../services/auth/auth_service.dart';
@@ -20,27 +22,28 @@ class _SocialAuthButtonsState extends State<SocialAuthButtons> {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
       children: [
-        SignInButton.mini(
+        SignInButton(
             buttonType: ButtonType.google,
             buttonSize: ButtonSize.medium,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+            width: 220.w,
+            padding: 7.w,
             elevation: 0,
             onPressed: _signInWithGoogle),
-        SignInButton.mini(
-            buttonType: ButtonType.apple,
+        height5Per(context: context),
+        SignInButton(
+            buttonType: ButtonType.appleDark,
             buttonSize: ButtonSize.medium,
-            elevation: 0,
-            onPressed: () {}),
-        SignInButton.mini(
-            buttonType: ButtonType.facebook,
-            buttonSize: ButtonSize.medium,
-            elevation: 0,
-            onPressed: () {}),
-        SignInButton.mini(
-            buttonType: ButtonType.github,
-            buttonSize: ButtonSize.medium,
+            width: 220.w,
+            padding: 7.w,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
             elevation: 0,
             onPressed: () {}),
       ],
