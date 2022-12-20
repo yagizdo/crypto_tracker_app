d66af6dcb3921e0fed736ef6ebcca6782080d136
Yilmaz Yagiz Dokumaci <dokumaciyagiz@gmail.com>
2022-12-21 00:16:58 +0300

    added: Inline change container widget on currency card

--- lib/widgets/currency/currency_card.dart
+++ lib/widgets/currency/currency_card.dart
@@ -11,17 +11,15 @@ class CurrencyCard extends StatelessWidget {
   final Currency currency;
   @override
   Widget build(BuildContext context) {
-
     // String currency change percentage to double
-    double? currencyChange = double.parse(currency.change!.replaceAll(',', '.').replaceAll('%', ''));
+    double? currencyChange =
+        double.parse(currency.change!.replaceAll(',', '.').replaceAll('%', ''));
 
     String? currencyName = currency.name?.replaceAll('-', ' ').toTitleCase();
 
-    if(currencyName!.length < 4) {
+    if (currencyName!.length < 4) {
       currencyName = currency.name?.replaceAll('-', ' ').toUpperCase();
-
     }
-
 
     return Padding(
       padding: EdgeInsets.symmetric(
@@ -44,36 +42,57 @@ class CurrencyCard extends StatelessWidget {
               Expanded(
                   flex: 2,
                   child: IconButton(
-                    onPressed: () {
-
-                    },
-                    icon: Icon(Icons.star_border, color: AppColors.blackBackground, size: 25.w,),
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.star_border,
+                      color: AppColors.blackBackground,
+                      size: 25.w,
+                    ),
                   )),
-              Expanded(flex: 3,child: Text(currencyName ?? 'No Data', maxLines: 2, style: AppTextStyle.currencyTitle(),)),
-              Expanded(flex : 3, child: Text(currency.buy.toString(), textAlign: TextAlign.start, style: AppTextStyle.currencyPrice(),)),
+              Expanded(
+                  flex: 3,
+                  child: Text(
+                    currencyName ?? 'No Data',
+                    maxLines: 2,
+                    style: AppTextStyle.currencyTitle(),
+                  )),
+              Expanded(
+                  flex: 3,
+                  child: Text(
+                    currency.buy.toString(),
+                    textAlign: TextAlign.start,
+                    style: AppTextStyle.currencyPrice(),
+                  )),
               // Currency change container
               Expanded(
                 flex: 4,
-                child: Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(8),
-                    color: currencyChange > 0 ? Colors.green : Colors.red,
-                  ),
-                  child: Padding(
-                    padding: EdgeInsets.symmetric(
-                      horizontal: 10.w,
-                      vertical: 5.w,
-                    ),
-                    child: Text(
-                      currency.change.toString(),
-                      textAlign: TextAlign.center,
-                      style: AppTextStyle.currencyChange(),
-                    ),
-                  ),
-                ),
+                child: _buildChangeContainer(currencyChange: currencyChange),
               ),
             ],
           ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildChangeContainer({required double? currencyChange}) {
+    currencyChange == null
+        ? currencyChange = 0
+        : currencyChange = currencyChange;
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(8),
+        color: currencyChange > 0 ? Colors.green : Colors.red,
+      ),
+      child: Padding(
+        padding: EdgeInsets.symmetric(
+          horizontal: 10.w,
+          vertical: 5.w,
+        ),
+        child: Text(
+          currency.change.toString(),
+          textAlign: TextAlign.center,
+          style: AppTextStyle.currencyChange(),
         ),
       ),
     );
