79149b18ed1b35235b69dd467bc30d193ae45735
Yilmaz Yagiz Dokumaci <dokumaciyagiz@gmail.com>
2022-12-20 17:36:03 +0300

    added: Currency Content comp

---
+++ lib/widgets/currency/currency_content.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CurrencyContent extends StatelessWidget {
+  const CurrencyContent({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
